9c5a0b4d175d921c60390eb976c92825e2a2b99a
joshiabhishek0392 <162970629+joshiabhishek0392@users.noreply.github.com>
2024-05-19 11:24:13 +0530

    Updated Q2.java

--- Assignment 7/Q2.java
+++ Assignment 7/Q2.java
@@ -1,22 +1,15 @@
 // Java Program to Implement Heaps
 // by Illustrating Min Heap
 
-// Main class (MinHeap)
 class GFG {
-
-	// Member variables of this class
 	private int[] Heap;
 	private int size;
 	private int maxsize;
 
-	// Initializing front as static with unity
 	private static final int FRONT = 1;
 
-	// Constructor of this class
 	public GFG(int maxsize)
 	{
-
-		// This keyword refers to current object itself
 		this.maxsize = maxsize;
 		this.size = 0;
 
@@ -24,32 +17,17 @@ class GFG {
 		Heap[0] = Integer.MIN_VALUE;
 	}
 
-	// Method 1
-	// Returning the position of
-	// the parent for the node currently
-	// at pos
 	private int parent(int pos) { return pos / 2; }
 
-	// Method 2
-	// Returning the position of the
-	// left child for the node currently at pos
 	private int leftChild(int pos) { return (2 * pos); }
 
-	// Method 3
-	// Returning the position of
-	// the right child for the node currently
-	// at pos
 	private int rightChild(int pos)
 	{
 		return (2 * pos) + 1;
 	}
 
-	// Method 4
-	// Returning true if the passed
-	// node is a leaf node
 	private boolean isLeaf(int pos)
 	{
-
 		if (pos > (size / 2)) {
 			return true;
 		}
@@ -57,8 +35,6 @@ class GFG {
 		return false;
 	}
 
-	// Method 5
-	// To swap two nodes of the heap
 	private void swap(int fpos, int spos)
 	{
 
@@ -69,15 +45,10 @@ class GFG {
 		Heap[spos] = tmp;
 	}
 
-	// Method 6
-	// To heapify the node at pos
 private void minHeapify(int pos)
 {	 
 	if(!isLeaf(pos)){
 	int swapPos= pos;
-	// swap with the minimum of the two children
-	// to check if right child exists. Otherwise default value will be '0'
-	// and that will be swapped with parent node. 
 	if(rightChild(pos)<=size)
 		swapPos = Heap[leftChild(pos)]<Heap[rightChild(pos)]?leftChild(pos):rightChild(pos);
 	else
@@ -91,8 +62,6 @@ private void minHeapify(int pos)
 	}	 
 }
 
-	// Method 7
-	// To insert a node into the heap
 	public void insert(int element)
 	{
 
@@ -109,26 +78,19 @@ private void minHeapify(int pos)
 		}
 	}
 
-	// Method 8
-	// To print the contents of the heap
 	public void print()
 	{
 		for (int i = 1; i <= size / 2; i++) {
 
-			// Printing the parent and both childrens
 			System.out.print(
 				" PARENT : " + Heap[i]
 				+ " LEFT CHILD : " + Heap[2 * i]
 				+ " RIGHT CHILD :" + Heap[2 * i + 1]);
 
-			// By here new line is required
 			System.out.println();
 		}
 	}
 
-	// Method 9
-	// To remove and return the minimum
-	// element from the heap
 	public int remove()
 	{
 
@@ -139,21 +101,13 @@ private void minHeapify(int pos)
 		return popped;
 	}
 
-	// Method 10
-	// Main driver method
 	public static void main(String[] arg)
 	{
 
-		// Display message
 		System.out.println("The Min Heap is ");
 
-		// Creating object of class in main() method
 		GFG minHeap = new GFG(15);
 
-		// Inserting element to minHeap
-		// using insert() method
-
-		// Custom input entries
 		minHeap.insert(5);
 		minHeap.insert(3);
 		minHeap.insert(17);
@@ -164,11 +118,8 @@ private void minHeapify(int pos)
 		minHeap.insert(22);
 		minHeap.insert(9);
 
-		// Print all elements of the heap
 		minHeap.print();
 
-		// Removing minimum value from above heap
-		// and printing it
 		System.out.println("The Min val is "
 						+ minHeap.remove());
 	}
